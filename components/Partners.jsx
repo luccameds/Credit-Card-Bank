106903e63928dd85c1f5002a6cbc5227cd3c4903
Lucca Medeiros <luccameds@gmail.com>
2023-05-18 22:44:45 -0300

    Created the Partners component

---
+++ components/Partners.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Partners = () => (
+  <div className="flex flex-row justify-center gap-[120px]">
+    <img src="logo-1.svg" alt="First Logo" />
+    <img src="logo-2.svg" alt="Second Logo" />
+    <img src="logo-3.svg" alt="Third Logo" />
+    <img src="logo-4.svg" alt="Fourth Logo" />
+  </div>
+);
+
+export default Partners;
