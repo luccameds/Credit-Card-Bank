6e1183196a7a71d93c23ee45937474bb96683223
Lucca Medeiros <luccameds@gmail.com>
2023-05-18 22:42:55 -0300

    Custom the page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Footer, Navbar } from '../components';
+import { Footer, Hero, Navbar, Partners } from '../components';
 
-const Page = () => {
-  return (
-    <div className="bg-primary-black overflow-hidden">
-      <Navbar />
-      <Footer />
-    </div>
-  );
-};
+const Page = () => (
+  <div className="bg-[#131170] px-[90px] py-[40px]">
+    <Navbar />
+    <div className="gradient-02 z-0" />
+    <Hero />
+    <div className="gradient-03 z-0" />
+    <Partners />
+    <div className="gradient-04 z-0" />
+    <Footer />
+  </div>
+);
 
 export default Page;
